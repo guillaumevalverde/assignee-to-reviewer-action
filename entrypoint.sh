fb19c736ffae222b09597d3b7dab4b6b3f3ea255
valverde Guillaume <guillaume.valverde@gmail.com>
2021-10-04 08:58:15 +0200

    Update entrypoint.sh
    protect quote

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,9 +22,11 @@ AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
 number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 reviewers=$(jq --raw-output '.pull_request.requested_reviewers|map(."login")' "$GITHUB_EVENT_PATH")
+reviewersWithQuote=reviewers.replace("\"", "\\\"")
 
 echo "set as reviewer: "
 echo "${reviewers}"
+echo "${reviewersWithQuote}"
 
 update_review_request() {
   curl -sSL \
@@ -32,7 +34,7 @@ update_review_request() {
     -H "${AUTH_HEADER}" \
     -H "${API_HEADER}" \
     -X $1 \
-    -d "{\"assignees\": ${reviewwers}}" \
+    -d "{\"assignees\": ${reviewersWithQuote}}" \
     "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${number}/assignees"
 }
 
