266c3059c17b017739f6cf1260945488be5c23b3
valverde Guillaume <guillaume.valverde@gmail.com>
2021-10-04 12:51:03 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,8 @@ number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 reviewers=$(jq --raw-output '.pull_request.requested_reviewers|map(."login")' "$GITHUB_EVENT_PATH")
 assignee=$(jq --raw-output .assignee.login "$GITHUB_EVENT_PATH")
 list_reviewers=${reviewers//\"/\\\"}
+listReviewerWithoutSpace=`echo "${list_reviewers}" | tr -d '[:space:]'`
+listReviewerWithoutSpace=`echo ${list_reviewers} | tr -d '[:space:]'`
 #listReviewerWithoutSpace = `echo ${list_reviewers} | tr -d '[:space:]'`
                             
 echo "set as reviewer: "
